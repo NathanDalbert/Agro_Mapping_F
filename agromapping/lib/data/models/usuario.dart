ad30cbdb618d2d5617bd09c58cc351b87b5ccd32
NathanDalbert <dalbert.nathan@gmail.com>
2025-08-13 16:27:06 -0300

    feat: adicionar classe Usuario com método de conversão de JSON e valor padrão para userRole

--- agromapping/lib/data/models/usuario.dart
+++ agromapping/lib/data/models/usuario.dart
@@ -1,8 +1,10 @@
+// lib/data/models/usuario.dart
+
 class Usuario {
   final String id;
   final String nome;
   final String email;
-  final String userRole;
+  final String userRole; // "USER", "SELLER", "ADMIN"
 
   Usuario({
     required this.id,
@@ -10,4 +12,16 @@ class Usuario {
     required this.email,
     required this.userRole,
   });
+
+  factory Usuario.fromJson(Map<String, dynamic> json) {
+    return Usuario(
+      id: json['id'],
+      nome: json['nome'],
+      email: json['email'],
+      userRole: json['userRole'] ?? 'USER', // Garante um valor padrão
+    );
+  }
+
+  // Helper para saber se o utilizador é um vendedor
+  bool get isSeller => userRole == 'SELLER';
 }
